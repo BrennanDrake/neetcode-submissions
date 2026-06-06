dd2995482c02df8cc76c58844aef24617886736e
Carvinaxe3 <81040749+BrennanDrake@users.noreply.github.com>
2026-06-06 15:09:27 -0700

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> stack;
+        for (const string& op: operations){
+            if (op == "+"){
+                int top = stack.back();
+                stack.pop_back();
+                int new_top = top + stack.back();
+                stack.push_back(top);
+                stack.push_back(new_top);
+            } else if(op == "D"){
+                stack.push_back(stack.back()*2);
+            } else if(op == "C"){
+                stack.pop_back();
+            } else {
+                stack.push_back(stoi(op));
+            }
+            
+        }
+        return accumulate(stack.begin(), stack.end(), 0);   
+    }
+};
