fc5f91f92242ce277d84311a194842458a971734
Carvinaxe3 <81040749+BrennanDrake@users.noreply.github.com>
2026-06-13 16:47:30 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *max_element(piles.begin(), piles.end());
+        int output = r;
+        //remember we are optimizing speed so if our condition is less than we need
+        // we can look for slower positions (lower value aka move in R pointer)
+
+
+        while(l <= r){
+            double m = ((r-l)/2)+l;
+            long long totalTime = 0;
+
+            for(double p : piles){
+                totalTime += ((p+m-1)/m);
+            }
+            if(totalTime <= h){
+                r = m-1;
+                int pot = m;
+            }else if(totalTime > h){
+                l = m+1;
+            }else{
+                return m;
+            }
+        }
+        return l;
+    }
+};
