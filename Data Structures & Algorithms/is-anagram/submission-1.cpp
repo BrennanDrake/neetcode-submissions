02b253035f7eebbc6bdb1110df661c4fd235c79c
Carvinaxe3 <81040749+BrennanDrake@users.noreply.github.com>
2026-07-05 10:34:43 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    std::unordered_map<char, int> chars_s;
+    std::unordered_map<char, int> chars_t;
+
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        for (char c : s){
+            chars_s[c] = chars_s[c]+1;
+        }
+        for (char c : t){
+            chars_t[c] = chars_t[c]+1;
+        }
+        for (const auto& [key, value] : chars_s) {
+            if (chars_s[key] != chars_t[key]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
